2d9f7252a71047a00261458bb4a8ebeadc276a66
Kiran Brahmatewari <kiwi2mii@gmail.com>
2025-11-18 17:00:03 -0500

    Add dev container stuff

---
+++ .devcontainer/post-create.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+if [ -f package.json ]; then
+  bash -i -c "nvm install --lts && nvm install-latest-npm"
+  npm i
+  npm run build
+fi
+
+# Install dependencies for shfmt extension
+curl -sS https://webi.sh/shfmt | sh &>/dev/null
+
+# Avoid git log use less
+echo -e "\nunset LESS" >>~/.bashrc

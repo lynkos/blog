14434f219f10e43890f70b1f3cac72947a2ba0a0
Kiran Brahmatewari <kiwi2mii@gmail.com>
2025-11-18 15:33:41 -0500

    Create liquid JavaScript file to generate header vals from `_config.yml`

---
+++ assets/js/data/toc-config.js
@@ -0,0 +1,18 @@
+---
+layout: compress
+---
+
+{% comment %}
+Generate heading selector string for table of contents.
+{% endcomment %}
+
+{% assign heading_levels = "" %}
+{% for i in (site.toc.min_heading_level..site.toc.max_heading_level) %}
+  {% if forloop.first %}
+    {% assign heading_levels = "h" | append: i %}
+  {% else %}
+    {% assign heading_levels = heading_levels | append: ", h" | append: i %}
+  {% endif %}
+{% endfor %}
+
+export const tocConfig = '{{ heading_levels }}';

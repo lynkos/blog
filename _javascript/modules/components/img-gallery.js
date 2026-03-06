fbbe1df43b41b80143b449967af6fa914902b7cc
Kiran Brahmatewari <kiwi2mii@gmail.com>
2026-03-05 20:01:41 -0500

    Fix rounded corner bug on iOS for image gallery

--- _javascript/modules/components/img-gallery.js
+++ _javascript/modules/components/img-gallery.js
@@ -2,25 +2,32 @@
  * Dynamically rounds corners of gallery images on load and window resize.
  */
 
-function roundCorners() {
-  document.querySelectorAll('.slides a.popup img').forEach(img => {
-    if (!img.complete) return;
+function roundCorners(img) {
+  if (!img.complete) return;
 
-    const parent = img.closest('.slideshow');
-    //console.log(`Image loaded: ${img.src}, natural size: ${img.clientWidth}x${img.clientHeight}, slideshow size: ${parent.clientWidth}x${parent.clientHeight}`);
+  const parent = img.closest('.slideshow');
+  //console.log(`Image loaded: ${img.src}, natural size: ${img.clientWidth}x${img.clientHeight}, slideshow size: ${parent.clientWidth}x${parent.clientHeight}`);
 
-    if (img.clientWidth === parent.clientWidth && img.clientHeight === parent.clientHeight) {
-      img.classList.add('fills-slideshow');
-    } else img.classList.remove('fills-slideshow');
-  });
+  if (img.clientWidth === parent.clientWidth && img.clientHeight === parent.clientHeight) {
+    img.classList.add('fills-slideshow');
+  } else img.classList.remove('fills-slideshow');
 }
 
 export function applyRoundedCorners() {
-  document.querySelectorAll('.slides a.popup img').forEach(img => {
-    img.onload = roundCorners;
+  const visibilityObserver = new IntersectionObserver((entries) => {
+    entries.forEach(entry => {
+      if (entry.isIntersecting) {
+        roundCorners(entry.target);
+      }
+    });
   });
 
-  window.addEventListener('resize', roundCorners);
+  document.querySelectorAll('.slides a.popup img').forEach(img => {
+    img.onload = () => roundCorners(img);
+    visibilityObserver.observe(img);
+  });
 
-  roundCorners();
+  window.addEventListener('resize', () => {
+    document.querySelectorAll('.slides a.popup img').forEach(img => roundCorners(img));
+  });
 }

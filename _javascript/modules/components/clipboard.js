c4ffd4fe366a63943a8f44238ca59dd7ae00890f
Kiran Brahmatewari <kiwi2mii@gmail.com>
2026-06-01 17:20:47 -0400

    Fix "Copied!" bug

--- _javascript/modules/components/clipboard.js
+++ _javascript/modules/components/clipboard.js
@@ -7,7 +7,7 @@
 
 import Tooltip from 'bootstrap/js/src/tooltip';
 
-const clipboardSelector = '.code-header>button';
+const clipboardSelector = '.code-header .copy-btn';
 
 const ICON_DEFAULT = 'far fa-clipboard';
 const ICON_SUCCESS = 'fas fa-check';
